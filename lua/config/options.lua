e5352a0ab8a79e05fc2a4563d388cf42b34b670a
Abdullah Al Khalid <khalidrafi1111@gmail.com>
2026-09-15 06:21:28 +0600

    added config for mac

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,3 +8,11 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 vim.opt.scrolloff = 8
+
+-- Ensure Apple Clang uses Xcode's SDK
+if vim.fn.has("mac") == 1 and not vim.env.SDKROOT then
+  local sdk = vim.fn.system("xcrun --sdk macosx --show-sdk-path 2>/dev/null"):gsub("%s+", "")
+  if sdk ~= "" then
+    vim.env.SDKROOT = sdk
+  end
+end
