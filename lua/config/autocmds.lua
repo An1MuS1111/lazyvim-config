df0f5b4cadd773360a91796ec07f2ca2bec97730
Abdullah Al Khalid <khalidrafi1111@gmail.com>
2026-09-17 18:39:48 +0600

    add config for apps

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,13 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("rust_disable_single_quote_pairs", { clear = true }),
+  pattern = "rust",
+  callback = function()
+    -- Overrides mini.pairs behavior to only insert a single quote
+    vim.keymap.set("i", "'", "'", { buffer = 0 })
+  end,
+  desc = "Disable single quote autopairs in Rust (lifetimes)",
+})
